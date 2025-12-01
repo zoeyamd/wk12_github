7e594a2cb2a61e802f66e745d47378e980e57f83
zoeyamd <zoey_dangleman@brown.edu>
2025-12-01 01:50:10 -0500

    Create estimation.R

---
+++ estimation.R
@@ -0,0 +1,93 @@
+#************************************* ESTIMATION ************************************#
+#                                                                                     #
+#                                                                                     #
+# This code generates the estimated arrival rates of the bike data sample.            #
+#*************************************************************************************#
+
+#loading libraries and reading in data
+library(tidyverse)
+bike_data <- read_csv("~/Downloads/1560/Data/sample_bike.csv")
+
+#initial function to estimate arrival rates
+estimate_arrival_rates <- function(data) {
+  
+  # compute the average number of trips per hour between each pair
+  x_hat <- data %>%
+    mutate(hour = hour(start_time)) %>%
+    filter(start_station != "R", end_station != "R") %>%
+    group_by(start_station, end_station, hour) %>%
+    summarise(avg_trips = n() / n_distinct(as_date(start_time)), 
+              .groups = "drop") 
+  
+  # pivot longer to get change in count 
+  data$end_station <- as.character(data$end_station)
+  trips_long <- data %>%
+    pivot_longer(cols = c("start_station", "start_time", 
+                          "end_station", "end_time"),
+                 names_to = c("type", ".value"),   
+                 names_pattern = "(start|end)_(.*)") %>%
+    mutate(change = ifelse(type == "start", -1, 1),
+           hour = hour(time)) %>%
+    select(station, time, hour, change)
+  
+  # add hour markers so we can get cumulative time
+  dates <- unique(as_date(trips_long$time))
+  hours <- c(seq(0,23,1),seq(0,23,1)+0.9999999)
+  stations <- unique(trips_long$station)
+  hr_pts <- expand.grid(time = dates, hour = hours, 
+                        station = stations) %>%
+    mutate(time = as.POSIXct(time) + hour*60*60,
+           hour = hour(time))
+  hr_pts$change <- 0
+  trips_long <- rbind(trips_long, hr_pts)
+  
+  # find average availability 
+  alpha_hat <- trips_long %>%
+    group_by(station) %>%
+    filter(station != "R") %>%
+    arrange(time) %>% 
+    mutate(count = cumsum(change),
+           date = as_date(time)) %>%
+    group_by(station, hour, date) %>%
+    summarize(time_avail = 
+                sum(difftime(time, lag(time), units="hours")*(count > 0), 
+                    na.rm = TRUE)) %>%
+    summarize(avg_avail = mean(time_avail)) %>%
+    mutate(avg_avail = round(as.numeric(avg_avail), digits = 4)) %>%
+    ungroup()
+  
+  # join the data and compute arrival rates
+  mu_hat <- x_hat %>%
+    left_join(alpha_hat, by = c("start_station" = "station", "hour")) %>%
+    mutate(mu_hat = ifelse(avg_avail > 0, avg_trips / avg_avail, NA))
+  
+  return(mu_hat)
+}
+
+#estimate arrival rates of bike sample
+arrival_rates <- estimate_arrival_rates(bike_data)
+
+#view the results
+print(arrival_rates, n = 10)
+
+#function to find lambda max of routes and complete hourly rates for each route
+find_lambda_max <- function(data){
+  lambda_maxes <- data %>%
+    group_by(start_station, end_station) %>%
+    
+    #complete time series and fill with 0s 
+    complete(hour = 0:23, fill = list(mu_hat = 0)) %>%
+    ungroup() %>%
+    group_by(start_station, end_station) %>%
+    mutate(lambda_max = max(mu_hat)) %>%
+    arrange(hour, start_station, end_station)
+  return(lambda_maxes)
+}
+
+#generate complete estimated arrival rates data frame
+complete_estimated_arrivals <- find_lambda_max(arrival_rates)
+
+#remove route without necessary data
+complete_estimated_arrivals <- complete_estimated_arrivals %>%
+                                    filter(start_station != "15")
+
